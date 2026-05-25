4f79c604f98d1663efe7189cce1bfaafc3db55b7
ShowStandards <redheadedcarrotfrommars@gmail.com>
2026-05-25 11:11:56 -0300

    Update cat-engine.js

--- cat-engine.js
+++ cat-engine.js
@@ -3,63 +3,34 @@
 ========================= */
 
 function runCatGenetics(inputs) {
-
   const mode = inputs.mode;
 
-  if (mode === "predictor") {
-    return runCatPredictor(inputs);
-  }
-
-  if (mode === "roll") {
-    return runCatRoll(inputs);
-  }
-
-  if (mode === "phenotypeFromGenotype") {
-    return runCatPhenotypeCalculator(inputs);
-  }
-
-  if (mode === "genotypeFromPhenotype") {
-    return runCatGenotypeBuilder(inputs);
-  }
+  if (mode === "predictor") return runCatPredictor(inputs);
+  if (mode === "roll") return runCatRoll(inputs);
+  if (mode === "phenotypeFromGenotype") return runCatPhenotypeCalculator(inputs);
+  if (mode === "genotypeFromPhenotype") return runCatGenotypeBuilder(inputs);
 
   return "Invalid cat genetics mode.";
-
 }
 
-/* =========================
-   MODE HANDLERS
-========================= */
-
 function runCatPredictor(inputs) {
-
   return `
     <h4>Cat Predictor</h4>
     <p>Cat predictor engine coming next.</p>
   `;
-
 }
 
 function runCatRoll(inputs) {
-
   return `
     <h4>Cat Roll</h4>
     <p>Cat kitten roller coming next.</p>
   `;
-
 }
 
 function runCatPhenotypeCalculator(inputs) {
-
-  const genotypeText =
-    inputs.singleGenotype;
-
-  const parsed =
-    parseCatGenotype(genotypeText);
-
-  console.log(parsed);
-
-  const phenotype =
-    getCatPhenotype(parsed);
+  const genotypeText = inputs.singleGenotype;
+  const parsed = parseCatGenotype(genotypeText);
+  const phenotype = getCatPhenotype(parsed);
 
   return renderCatResults(
     "Cat Phenotype Calculator",
@@ -68,16 +39,13 @@ function runCatPhenotypeCalculator(inputs) {
       <p><b>Genotype:</b> ${genotypeText}</p>
     `
   );
-
 }
 
 function runCatGenotypeBuilder(inputs) {
-
   return `
     <h4>Cat Genotype Builder</h4>
     <p>Cat genotype builder coming next.</p>
   `;
-
 }
 
 /* =========================
@@ -85,82 +53,115 @@ function runCatGenotypeBuilder(inputs) {
 ========================= */
 
 function parseCatGenotype(genotypeText) {
-
   const text = String(genotypeText || "")
     .replace(/\s+/g, " ")
     .trim();
 
   return {
+    Orange: findCatOrangeGene(text),
 
-    Orange:
-      findCatOrangeGene(text),
+    Agouti: findCatGenePair(
+      text,
+      ["A/A", "A/a", "a/a"],
+      "a/a"
+    ),
 
-    Agouti:
-      findCatGenePair(
-        text,
-        ["A/A", "A/a", "a/a"],
-        "a/a"
-      ),
+    Brown: findCatBrownGene(text),
 
-    Brown:
-      findCatBrownGene(text),
+    Dilute: findCatGenePair(
+      text,
+      ["D/D", "D/d", "d/d"],
+      "D/D"
+    ),
 
-    Dilute:
-      findCatGenePair(
-        text,
-        ["D/D", "D/d", "d/d"],
-        "D/D"
-      ),
+    White: findCatGenePair(
+      text,
+      ["W/W", "W/w", "w/w"],
+      "w/w"
+    ),
 
-    White:
-      findCatGenePair(
-        text,
-        ["W/W", "W/w", "w/w"],
-        "w/w"
-      ),
+    WhiteSpotting: findCatGenePair(
+      text,
+      ["S/S", "S/s", "s/s"],
+      "s/s"
+    ),
 
-    WhiteSpotting:
-      findCatGenePair(
-        text,
-        ["S/S", "S/s", "s/s"],
-        "s/s"
-      ),
+    Silver: findCatGenePair(
+      text,
+      ["I/I", "I/i", "i/i"],
+      "i/i"
+    ),
 
-    Silver:
-      findCatGenePair(
-        text,
-        ["I/I", "I/i", "i/i"],
-        "i/i"
-      ),
+    Colourpoint: findCatColourpointGene(text),
 
-    Colourpoint:
-      findCatColourpointGene(text),
+    Tabby: findCatTabbyGene(text),
 
-    Tabby:
-      findCatTabbyGene(text),
+    Spotted: findCatGenePair(
+      text,
+      ["Sp/Sp", "Sp/sp", "sp/sp"],
+      "sp/sp"
+    ),
 
-    Spotted:
-      findCatGenePair(
-        text,
-        ["Sp/Sp", "Sp/sp", "sp/sp"],
-        "sp/sp"
-      ),
+    Ticked: findCatGenePair(
+      text,
+      ["Ta/Ta", "Ta/ta", "ta/ta"],
+      "ta/ta"
+    ),
 
-Ticked:
-  findCatGenePair(
-    text,
-    ["Ta/Ta", "Ta/ta", "ta/ta"],
-    "ta/ta"
-  ),
+    Polydactyl: findCatGenePair(
+      text,
+      ["Pd/Pd", "Pd/pd", "pd/pd"],
+      "pd/pd"
+    ),
 
-Polydactyl:
-  findCatGenePair(
-    text,
-    ["Pd/Pd", "Pd/pd", "pd/pd"],
-    "pd/pd"
-  )
+    Amber: findCatGenePair(
+      text,
+      ["Amb/Amb", "Amb/n", "n/Amb", "n/n"],
+      "n/n"
+    ),
+
+    Sunshine: findCatGenePair(
+      text,
+      ["Su/Su", "Su/n", "n/Su", "n/n"],
+      "n/n"
+    ),
+
+    ExtremeSunshine: findCatGenePair(
+      text,
+      ["Es/Es", "Es/n", "n/Es", "n/n"],
+      "n/n"
+    ),
+
+    Charcoal: findCatGenePair(
+      text,
+      ["Ch/Ch", "Ch/n", "n/Ch", "n/n"],
+      "n/n"
+    ),
+
+    Wideband: findCatGenePair(
+      text,
+      ["Wb/Wb", "Wb/n", "n/Wb", "n/n"],
+      "n/n"
+    ),
+
+    Rufousing: findCatGenePair(
+      text,
+      ["Rf/Rf", "Rf/n", "n/Rf", "n/n"],
+      "n/n"
+    ),
+
+    Glitter: findCatGenePair(
+      text,
+      ["Gl/Gl", "Gl/n", "n/Gl", "n/n"],
+      "n/n"
+    ),
+
+    Karpati: findCatGenePair(
+      text,
+      ["Kp/Kp", "Kp/n", "n/Kp", "n/n"],
+      "n/n"
+    )
   };
-
 }
 
 /* =========================
@@ -168,39 +169,24 @@ Polydactyl:
 ========================= */
 
 function getCatPhenotype(parsed) {
-
-  // White overrides everything
-
   if (
     parsed.White === "W/W" ||
     parsed.White === "W/w"
   ) {
-    return "White";
+    return applyCatPolydactyl("White", parsed);
   }
 
-  let colour =
-    getCatBaseColour(parsed);
+  let colour = getCatBaseColour(parsed);
 
-  colour =
-    applyCatDilute(colour, parsed);
-
-  colour =
-    applyCatPointModifier(colour, parsed);
-
-  colour =
-    applyCatSilver(colour, parsed);
-
-  colour =
-    applyCatTabby(colour, parsed);
-
-colour =
-  applyCatWhiteSpotting(colour, parsed);
-
-colour =
-  applyCatPolydactyl(colour, parsed);
+  colour = applyCatDilute(colour, parsed);
+  colour = applyCatPointModifier(colour, parsed);
+  colour = applyCatSilver(colour, parsed);
+  colour = applyCatColourModifiers(colour, parsed);
+  colour = applyCatTabby(colour, parsed);
+  colour = applyCatWhiteSpotting(colour, parsed);
+  colour = applyCatPolydactyl(colour, parsed);
 
   return colour.trim();
-
 }
 
 /* =========================
@@ -208,47 +194,21 @@ colour =
 ========================= */
 
 function getCatBaseColour(parsed) {
-
-  // MALES
-
-  if (parsed.Orange === "O/Y") {
-    return "Red";
-  }
+  if (parsed.Orange === "O/Y") return "Red";
 
   if (parsed.Orange === "o/Y") {
-
-    if (parsed.Brown === "b/b") {
-      return "Chocolate";
-    }
-
-    if (parsed.Brown === "bl/bl") {
-      return "Cinnamon";
-    }
-
+    if (parsed.Brown === "b/b") return "Chocolate";
+    if (parsed.Brown === "bl/bl") return "Cinnamon";
     return "Black";
-
   }
 
-  // FEMALES
+  if (parsed.Orange === "O/O") return "Red";
+  if (parsed.Orange === "O/o") return "Tortie";
 
-  if (parsed.Orange === "O/O") {
-    return "Red";
-  }
-
-  if (parsed.Orange === "O/o") {
-    return "Tortie";
-  }
-
-  if (parsed.Brown === "b/b") {
-    return "Chocolate";
-  }
-
-  if (parsed.Brown === "bl/bl") {
-    return "Cinnamon";
-  }
+  if (parsed.Brown === "b/b") return "Chocolate";
+  if (parsed.Brown === "bl/bl") return "Cinnamon";
 
   return "Black";
-
 }
 
 /* =========================
@@ -256,137 +216,129 @@ function getCatBaseColour(parsed) {
 ========================= */
 
 function applyCatDilute(colour, parsed) {
+  if (parsed.Dilute !== "d/d") return colour;
 
-  if (parsed.Dilute !== "d/d") {
-    return colour;
-  }
-
-  if (colour === "Black") {
-    return "Blue";
-  }
-
-  if (colour === "Chocolate") {
-    return "Lilac";
-  }
-
-  if (colour === "Cinnamon") {
-    return "Fawn";
-  }
-
-  if (colour === "Red") {
-    return "Cream";
-  }
-
-  if (colour === "Tortie") {
-    return "Blue Tortie";
-  }
+  if (colour === "Black") return "Blue";
+  if (colour === "Chocolate") return "Lilac";
+  if (colour === "Cinnamon") return "Fawn";
+  if (colour === "Red") return "Cream";
+  if (colour === "Tortie") return "Blue Tortie";
 
   return colour;
-
 }
 
 function applyCatPointModifier(colour, parsed) {
+  const point = parsed.Colourpoint;
 
-  const point =
-    parsed.Colourpoint;
+  if (point === "ca/ca") return "Blue-Eyed Albino";
+  if (point === "c/c") return "Red-Eyed Albino";
 
-  // Albino overrides
-
-  if (point === "ca/ca") {
-    return "Blue-Eyed Albino";
-  }
-
-  if (point === "c/c") {
-    return "Red-Eyed Albino";
-  }
-
-  // Burmese
-
-  if (point === "cb/cb") {
-    return colour + " Burmese";
-  }
-
-  // Tonkinese
-
-  if (point === "cb/cs") {
-    return colour + " Mink";
-  }
-
-  // Siamese
+  if (point === "cb/cb") return colour + " Burmese";
+  if (point === "cb/cs") return colour + " Mink";
 
   if (point === "cs/cs") {
-
-    if (colour === "Black") {
-      return "Seal Point";
-    }
-
-    if (colour === "Blue") {
-      return "Blue Point";
-    }
-
-    if (colour === "Chocolate") {
-      return "Chocolate Point";
-    }
-
-    if (colour === "Lilac") {
-      return "Lilac Point";
-    }
-
-    if (colour === "Red") {
-      return "Flame Point";
-    }
-
-    if (colour === "Cream") {
-      return "Cream Point";
-    }
-
-    if (colour === "Tortie") {
-      return "Tortie Point";
-    }
-
-    if (colour === "Blue Tortie") {
-      return "Blue Tortie Point";
-    }
+    if (colour === "Black") return "Seal Point";
+    if (colour === "Blue") return "Blue Point";
+    if (colour === "Chocolate") return "Chocolate Point";
+    if (colour === "Lilac") return "Lilac Point";
+    if (colour === "Red") return "Flame Point";
+    if (colour === "Cream") return "Cream Point";
+    if (colour === "Tortie") return "Tortie Point";
+    if (colour === "Blue Tortie") return "Blue Tortie Point";
 
     return colour + " Point";
-
   }
 
   return colour;
-
 }
 
 function applyCatSilver(colour, parsed) {
-
   if (
-    parsed.Silver !== "I/I" &&
-    parsed.Silver !== "I/i"
+    parsed.Silver === "I/I" ||
+    parsed.Silver === "I/i"
+  ) {
+    return "Silver";
+  }
+
+  return colour;
+}
+
+function applyCatColourModifiers(colour, parsed) {
+  if (
+    colour === "Blue-Eyed Albino" ||
+    colour === "Red-Eyed Albino" ||
+    colour === "White"
   ) {
     return colour;
   }
 
-  // Tabbies become Silver Tabbies later
+  const modifiers = [];
 
   if (
-    parsed.Agouti === "A/A" ||
-    parsed.Agouti === "A/a"
+    parsed.Amber === "Amb/Amb" ||
+    parsed.Amber === "Amb/n"
   ) {
-    return "Silver " + colour;
+    modifiers.push("Amber");
   }
 
-  // Solids become Smoke
+  if (
+    parsed.ExtremeSunshine === "Es/Es" ||
+    parsed.ExtremeSunshine === "Es/n"
+  ) {
+    modifiers.push("Extreme Sunshine");
+  } else if (
+    parsed.Sunshine === "Su/Su" ||
+    parsed.Sunshine === "Su/n"
+  ) {
+    modifiers.push("Sunshine");
+  }
 
-  return colour + " Smoke";
+  if (
+    parsed.Charcoal === "Ch/Ch" ||
+    parsed.Charcoal === "Ch/n"
+  ) {
+    modifiers.push("Charcoal");
+  }
 
+  if (
+    parsed.Wideband === "Wb/Wb" ||
+    parsed.Wideband === "Wb/n"
+  ) {
+    modifiers.push("Shaded");
+  }
+
+  if (
+    parsed.Rufousing === "Rf/Rf" ||
+    parsed.Rufousing === "Rf/n"
+  ) {
+    modifiers.push("Rufoused");
+  }
+
+  if (
+    parsed.Glitter === "Gl/Gl" ||
+    parsed.Glitter === "Gl/n"
+  ) {
+    modifiers.push("Glitter");
+  }
+
+  if (
+    parsed.Karpati === "Kp/Kp" ||
+    parsed.Karpati === "Kp/n"
+  ) {
+    modifiers.push("Karpati");
+  }
+
+  if (modifiers.length > 0) {
+    return modifiers.join(" ") + " " + colour;
+  }
+
+  return colour;
 }
 
 function applyCatTabby(colour, parsed) {
+  if (parsed.Agouti === "a/a") return colour;
 
-  if (parsed.Agouti === "a/a") {
-    return colour;
-  }
-
-  let pattern = "Tabby";
+  let pattern = "Mackerel Tabby";
 
   if (
     parsed.Ticked === "Ta/Ta" ||
@@ -402,50 +354,31 @@ function applyCatTabby(colour, parsed) {
     pattern = "Spotted Tabby";
   }
 
-  else if (
-    parsed.Tabby === "mc/mc"
-  ) {
+  else if (parsed.Tabby === "mc/mc") {
     pattern = "Classic Tabby";
   }
 
-  else {
-    pattern = "Mackerel Tabby";
-  }
-
   return colour + " " + pattern;
-
 }
 
 function applyCatWhiteSpotting(colour, parsed) {
-
   const hasWhite =
     parsed.WhiteSpotting === "S/S" ||
     parsed.WhiteSpotting === "S/s";
 
-  if (!hasWhite) {
-    return colour;
-  }
+  if (!hasWhite) return colour;
 
-  // Torties with white become Calico
-
-  if (colour === "Tortie") {
-    return "Calico";
-  }
-
-  if (colour === "Blue Tortie") {
-    return "Dilute Calico";
-  }
+  if (colour === "Tortie") return "Calico";
+  if (colour === "Blue Tortie") return "Dilute Calico";
 
   if (parsed.WhiteSpotting === "S/S") {
     return colour + " High White";
   }
 
   return colour + " White";
-
 }
 
 function applyCatPolydactyl(colour, parsed) {
-
   if (
     parsed.Polydactyl === "Pd/Pd" ||
     parsed.Polydactyl === "Pd/pd"
@@ -454,19 +387,17 @@ function applyCatPolydactyl(colour, parsed) {
   }
 
   return colour;
-
 }
+
 /* =========================
    OUTPUT HELPERS
 ========================= */
 
 function renderCatResults(title, html) {
-
   return `
     <h4>${title}</h4>
     ${html}
   `;
-
 }
 
 /* =========================
@@ -474,48 +405,39 @@ function renderCatResults(title, html) {
 ========================= */
 
 function hasCatGene(pair, gene) {
-
   return String(pair || "")
     .split("/")
     .includes(gene);
-
 }
 
 function findCatGenePair(text, options, fallback) {
-
   const cleanText = String(text || "")
     .replace(/\s+/g, " ")
     .trim();
 
-  const tokens =
-    cleanText.split(" ");
+  const tokens = cleanText.split(" ");
 
   for (const option of options) {
-
-    const compactOption =
-      option.replace(/\//g, "");
+    const compactOption = option.replace(/\//g, "");
 
     for (const token of tokens) {
-
-      const compactToken =
-        token.replace(/\//g, "");
+      const compactToken = token.replace(/\//g, "");
 
       if (
         token === option ||
         token === compactOption ||
         compactToken === compactOption
       ) {
+        if (option.startsWith("n/")) {
+          return option.split("/").reverse().join("/");
+        }
 
         return option;
-
       }
-
     }
-
   }
 
   return fallback;
-
 }
 
 /* =========================
@@ -523,14 +445,12 @@ function findCatGenePair(text, options, fallback) {
 ========================= */
 
 function findCatOrangeGene(text) {
-
   const tokens = String(text || "")
     .replace(/\s+/g, " ")
     .trim()
     .split(" ");
 
   for (const token of tokens) {
-
     if (token === "O/Y") return "O/Y";
     if (token === "o/Y") return "o/Y";
 
@@ -538,22 +458,18 @@ function findCatOrangeGene(text) {
     if (token === "O/o") return "O/o";
     if (token === "o/O") return "O/o";
     if (token === "o/o") return "o/o";
-
   }
 
   return "o/o";
-
 }
 
 function findCatBrownGene(text) {
-
   const tokens = String(text || "")
     .replace(/\s+/g, " ")
     .trim()
     .split(" ");
 
   for (const token of tokens) {
-
     if (token === "B/B") return "B/B";
     if (token === "B/b") return "B/b";
     if (token === "b/B") return "B/b";
@@ -567,41 +483,23 @@ function findCatBrownGene(text) {
     if (token === "bl/b") return "b/bl";
 
     if (token === "bl/bl") return "bl/bl";
-
   }
 
   return "B/B";
-
 }
 
 function findCatColourpointGene(text) {
-
   const tokens = String(text || "")
     .replace(/\s+/g, " ")
     .trim()
     .split(" ");
 
   for (const token of tokens) {
+    if (token === "C/C") return "C/C";
 
-    // Full colour
+    if (token === "cb/cb") return "cb/cb";
 
-    if (token === "C/C") {
-      return "C/C";
-    }
-
-    // Burmese
-
-    if (token === "cb/cb") {
-      return "cb/cb";
-    }
-
-    // Siamese
-
-    if (token === "cs/cs") {
-      return "cs/cs";
-    }
-
-    // Tonkinese
+    if (token === "cs/cs") return "cs/cs";
 
     if (
       token === "cb/cs" ||
@@ -610,42 +508,29 @@ function findCatColourpointGene(text) {
       return "cb/cs";
     }
 
-    // Albino
-
-    if (token === "ca/ca") {
-      return "ca/ca";
-    }
-
-    if (token === "c/c") {
-      return "c/c";
-    }
-
+    if (token === "ca/ca") return "ca/ca";
+    if (token === "c/c") return "c/c";
   }
 
   return "C/C";
-
 }
 
 function findCatTabbyGene(text) {
-
   const tokens = String(text || "")
     .replace(/\s+/g, " ")
     .trim()
     .split(" ");
 
   for (const token of tokens) {
-
     if (token === "Mc/Mc") return "Mc/Mc";
 
     if (token === "Mc/mc") return "Mc/mc";
     if (token === "mc/Mc") return "Mc/mc";
 
     if (token === "mc/mc") return "mc/mc";
-
   }
 
   return "Mc/Mc";
-
 }
 
 window.runCatGenetics = runCatGenetics;
